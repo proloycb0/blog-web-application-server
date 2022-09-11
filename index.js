17f5dd787caedbf73dc9788ed604727829077dc6
Proloycb <proloychacroborty@gmail.com>
2022-09-11 19:05:55 +0600

    connect with mongodb

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+// connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1xlxnhx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        const blogsCollection = client.db('blogUser').collection('blogs');
+        const usersCollection = client.db('blogUser').collection('users');
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, token });
+        });
+    } finally {
+        
+    }
+}
+
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello from kep blogger application')
+});
+
+app.listen(port, () => {
+    console.log(`Kep Blogger app listening on port ${port}`)
+});
