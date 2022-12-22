8bfbf746b315b9d01fccdb0abc6610ba5c916719
Proloycb <proloychacroborty@gmail.com>
2022-12-22 17:59:37 +0600

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // connect mongodb
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1xlxnhx.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ovgfiq2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
